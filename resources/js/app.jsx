e8e16e23f9db3818cf8b27a2ac598338e6ed23ef
Cody Welsh <codyw@protonmail.com>
2020-10-22 18:33:48 -0700

    feat(JS): Implement basic React front-end
    Modify the prebuilt Webpack `mix` file to include `react`. Add
    appropriate `npm` packages. Rename `app.js` -> `app.jsx`. Create
    barebones `blade` view to host the front-end.

---
+++ resources/js/app.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render(<div>Lorem Ipsum</div>, document.getElementById('root'));
---
+++ resources/views/app.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="{{app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,
+        initial-scale=1">
+        <title>Minuet Patients</title>
+    </head>
+    <body>
+        <div id="root"></div>
+        <script src="{{ asset('js/app.js') }}" ></script>
+    </body>
+</html>
