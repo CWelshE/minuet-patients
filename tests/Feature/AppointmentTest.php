a802780bf4fff5e69dcf186660b61cc7a1685278
Cody Welsh <codyw@protonmail.com>
2020-10-24 14:02:26 -0700

    chore(Tests): Delete example tests

--- tests/Feature/AppointmentTest.php
+++ tests/Feature/AppointmentTest.php
@@ -11,11 +11,16 @@ class AppointmentTest extends TestCase
      *
      * @return void
      */
-    public function testAppointmentsNotEmpty()
+    public function testAppointmentsNotEmptyForValidRequest()
     {
         $response = $this->get('/api/patients/1/appts');
         $respContent = $response->decodeResponseJson();
         $this->assertGreaterThan(0, count($respContent));
         $response->assertOk();
+    }
+
+    public function testAppointmentsEmptyForInvalidRequest() {
+        $reponse = $this->get('/api/patients/invalid/appts');
+        $reponse->assertStatus(500);
     }
 }
