35c8f9a3a5d873a09c5389af36de71a8a0992f99
Cody Welsh <codyw@protonmail.com>
2020-10-25 19:23:25 -0700

    feat(Patients): Style Patient/slist
    Style both the Patient and PatientsList components with emotion. Some
    Appointments/AppointmentsList CSS is controlled at the top-level here.

--- resources/js/components/Patients/Patient.jsx
+++ resources/js/components/Patients/Patient.jsx
@@ -1,12 +1,42 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
+import styles from "../../styles/constants.js";
+
+const patientStyles = css`
+  display: flex;
+  flex-direction: column;
+  cursor: pointer;
+  background-color: ${styles.colors.primary};
+  padding: 1rem;
+  border: 1px solid ${styles.colors.secondary};
+  & h3 {
+    font-size: ${styles.typography.header};
+  }
+
+  & span {
+    font-size: ${styles.typography.caption};
+  }
+
+  & h3,
+  span {
+    color: white;
+  }
+
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.8);
+  }
+  transition: all 0.15s;
+`;
+
 /**
  * Represents a single patient in a PatientsList. Presentational
  * component only.
  */
 const Patient = (props) => (
-  <div onClick={props.handleClick} className={"patient"}>
+  <div onClick={props.handleClick} className={"patient"} css={patientStyles}>
     <h3>
       {props.firstName} {props.lastName}
     </h3>
@@ -20,6 +50,7 @@ Patient.propTypes = {
   lastName: PropTypes.string.isRequired,
   dob: PropTypes.string.isRequired,
   phone: PropTypes.string.isRequired,
+  handleClick: PropTypes.func.isRequired,
 };
 
 export default Patient;
--- resources/js/components/Patients/PatientsList.jsx
+++ resources/js/components/Patients/PatientsList.jsx
@@ -2,6 +2,41 @@ import React, { useState, useEffect } from "react";
 import Patient from "./Patient.jsx";
 import AppointmentsList from "../Appointments/AppointmentsList.jsx";
 
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
+import styles from "../../styles/constants.js";
+import "fontsource-roboto";
+
+const containerStyles = css`
+  display: flex;
+  flex-direction: row;
+  height: calc(100vh - 4rem);
+  padding: 2rem;
+  width: calc(95vw - 4rem);
+  align-self: center;
+`;
+
+const patientsStyles = css`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  overflow-y: scroll;
+  font-family: ${styles.typography.fontFamily};
+  scrollbar-width: thin;
+  border-top-left-radius: 5px;
+  border-bottom-left-radius: 5px;
+`;
+
+const appointmentsStyles = css`
+  flex: 2;
+  overflow-y: scroll;
+  scrollbar-width: none;
+  background-color: ${styles.colors.primary};
+  font-family: ${styles.typography.fontFamily};
+  border-top-right-radius: 5px;
+  border-bottom-right-radius: 5px;
+`;
+
 /**
  * List all available `Patient`s. Container component.
  */
@@ -37,9 +72,13 @@ const PatientsList = () => {
   // Flash loading text prior to fetch() completion
   const isLoading = patients.length === 0;
   return (
-    <div className="patients-list">
-      <ul>{isLoading ? "Loading Patients.." : patientItems}</ul>
-      <AppointmentsList patientId={selectedPatient["id"]} />
+    <div css={containerStyles}>
+      <ul css={patientsStyles}>
+        {isLoading ? "Loading Patients.." : patientItems}
+      </ul>
+      <ul css={appointmentsStyles}>
+        <AppointmentsList patient={selectedPatient} />
+      </ul>
     </div>
   );
 };
