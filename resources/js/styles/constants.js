bda15a8ebdc1267b08072d78a35a6f68f1b55fb0
Cody Welsh <codyw@protonmail.com>
2020-10-25 19:20:41 -0700

    style(constants): Add constants.js
    Add a constants file for commonly used CSS properties, such as
    application background colors.

---
+++ resources/js/styles/constants.js
@@ -0,0 +1,20 @@
+// Stylistic constants and helper functions for CSS.
+const typography = {
+  header: "24px",
+  headerSmall: "17px",
+  body: "15px",
+  caption: "14px",
+  fontFamily: "Roboto",
+};
+
+const colors = {
+  primary: "#004643",
+  secondary: "#92BFB1",
+};
+
+const styles = {
+  typography,
+  colors,
+};
+
+export default styles;
